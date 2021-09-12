7d032a16b3d2d22aabecc7e367aff78f2b1baabc
Bảo Đặng Kim <dangkimbao1999@gmail.com>
2021-09-12 22:48:57 +0700

    add gradient background

--- cv_vipro/src/components/App.js
+++ cv_vipro/src/components/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import NavBar from './Navigation/NavBar'
 import './../config/styles.scss'
 import { BrowserRouter as Router, Switch, Route, BrowserRouter } from "react-router-dom";
+import TransformBackground from "./TransformBackground";
 const App = () => {
     
     return (
         <>
         <Router>
             <NavBar pageType="bao_dang/homepage"/>
+            <TransformBackground/>
         </Router>
         </>
     );
