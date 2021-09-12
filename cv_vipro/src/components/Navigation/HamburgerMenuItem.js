0a2da6835468342ce22746e384b4242fd1880fd7
Bảo Đặng Kim <dangkimbao1999@gmail.com>
2021-09-12 23:40:22 +0700

    styling the menu-items

--- cv_vipro/src/components/Navigation/HamburgerMenuItem.js
+++ cv_vipro/src/components/Navigation/HamburgerMenuItem.js
@@ -5,10 +5,11 @@ function HamburgerMenuItem({pageTitle, pageTitleColor, shortDescription, imagePa
         color: {pageTitleColor},
         fontWeight: "bold",
         fontSize: "42px",
+        fontFamily: "'Manrope',sans-serif"
     }
     return (
         <div className="container">
-            <div className="row my-4">
+            <div className="row my-4 align-items-center">
                 <img src={imagePath} className="img-fluid col-3 ml-4" align="center" alt="Decorative image"/>
                 <div className="text col-8">
                     <h1 className="lead" style={titleStyle}>{pageTitle}</h1>
