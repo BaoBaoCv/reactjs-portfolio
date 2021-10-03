725c68a1fc7b63a6566c1d05b47f67f1da89d0ec
Bảo Đặng Kim <dangkimbao1999@gmail.com>
2021-10-03 09:48:43 +0700

    design motivation page

--- src/components/pages/motivation/Motivation.jsx
+++ src/components/pages/motivation/Motivation.jsx
@@ -7,7 +7,7 @@ import Saly from "@/assets/images/Saly-16.png"
 const Motivation = () => {
     return (
         <Row>
-            <Col className="motivation">
+            <Col className="motivation" xl={6} md={12}>
                 <h1>Thank you for blabla blabla</h1>
                 <p>{`Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
             when an unknown printer took a galley of type and scrambled it to make a type specimen book.
@@ -15,8 +15,8 @@ const Motivation = () => {
              It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, \n
               and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum`}</p>
             </Col>
-            <Col>
-                <img src={Saly} />
+            <Col xl={6} md={12}>
+                <img src={Saly} className="motivation__image" />
 
             </Col>
         </Row>
