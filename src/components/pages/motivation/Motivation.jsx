fb2d795a84479ad697b904a30da916eab444869f
Bảo Đặng Kim <dangkimbao1999@gmail.com>
2021-10-03 09:35:24 +0700

    update motivation page

--- src/components/pages/motivation/Motivation.jsx
+++ src/components/pages/motivation/Motivation.jsx
@@ -1,18 +1,25 @@
 import React, { useEffect, useState } from "react";
 import "./motivation.scss";
+import { Col, Container, Row } from "react-bootstrap";
+import Saly from "@/assets/images/Saly-16.png"
 
 
 const Motivation = () => {
     return (
-        <div className="motivation">
-            <h1>Thank you for blabla blabla</h1>
-            <p>{`Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+        <Row>
+            <Col className="motivation">
+                <h1>Thank you for blabla blabla</h1>
+                <p>{`Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
             when an unknown printer took a galley of type and scrambled it to make a type specimen book.
              It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
              It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, \n
               and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum`}</p>
-        </div>
+            </Col>
+            <Col>
+                <img src={Saly} />
+
+            </Col>
+        </Row>
     )
 };
 
