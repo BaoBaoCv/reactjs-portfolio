a1752b1bb41c7b9886f29626dd94affa735d97fb
Bảo Đặng Kim <dangkimbao1999@gmail.com>
2021-10-03 20:16:19 +0700

    test rebase

--- src/components/pages/motivation/Motivation.jsx
+++ src/components/pages/motivation/Motivation.jsx
@@ -8,12 +8,10 @@ const Motivation = () => {
     return (
         <Row>
             <Col className="motivation" xl={6} md={12}>
-                <h1>Thank you for blabla blabla</h1>
-                <p>{`Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-            when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-             It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
-             It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, \n
-              and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum`}</p>
+                <h1>Thank you for visiting my site</h1>
+                <p>{`First of all, I just want to say thank you to all of the person participated in this project. trước hết, tôi muốn gửi lời cảm ơn tới tất những người đã tham gia vào project này. Project này chỉ có thể được hoàn thiện và đạt the edge of my imagination only because of your participation in this project. Một lần nữa tôi xin cảm ơn
+
+As a straight-out-of-school developer (actually not yet straight-out-of-school when this project was completed), thì việc đi làm có thể khiến một con người luôn cần sự inspiration như tôi to become productive, cảm thấy mệt mỏi. That's why I spent a few hours everyday, colaberating with my teammate (which re-fueled me a lot), and make this exceptional site. I have been always want to do something that is more inspired, connect people,   `}</p>
             </Col>
             <Col xl={6} md={12}>
                 <img src={Saly} className="motivation__image" />
