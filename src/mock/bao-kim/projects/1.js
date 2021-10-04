0e6f7b74365033e2e2a28c1f50e8d0f6b10d6aed
Bảo Đặng Kim <dangkimbao1999@gmail.com>
2021-10-04 22:32:31 +0700

    content 1 and 2

--- src/mock/bao-kim/projects/1.js
+++ src/mock/bao-kim/projects/1.js
@@ -1,76 +1,37 @@
-import { getRndInteger, PIXEL_ARTS } from "@/constants";
+import placeholder from "@/mock/bao-kim/images/placeholder.jpg";
+import { CHARACTER_IMAGES, getRndInteger, PIXEL_ARTS } from "@/constants";
 
 export default {
-    name: "Android Application Robot",
-    client: "Google Inc",
+    name: "Games simulating on Minecraft",
+    client: "School subject",
     role: "Developer",
     date: new Date(2018, 1),
     tags: ["python", "backend"],
     contents: [
         {
             component: "ProjectDescription",
-            title: "Overview",
-            col1: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's djwaiojd iowaj iodjawid joaiwjd ioajid jawiodjiawjdia
-            standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-            It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
-            It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
-            and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
-            "discovered the undoubtable source.Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of \n de Finibus Bonorum et Malorum 
-            (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. 
-            The first line of Lorem Ipsum, Lorem ipsum dolor sit amet.., comes from a line in section`,
-            col2: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.\n\n It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum`,
-        },
-        {
-            component: "ImageWithTitle",
-            src1: PIXEL_ARTS[getRndInteger(0, PIXEL_ARTS.length - 1)],
-            src2: PIXEL_ARTS[getRndInteger(0, PIXEL_ARTS.length - 1)],
-            description1: "desc1",
-            description2: "desc2",
-        },
-        {
-            component: "VideoWithTitle",
-            src: `https://www.youtube.com/watch?v=AgFeZr5ptV8&list=RDAgFeZr5ptV8&start_radio=1`,
-            description: `First`,
+            title: "GOAL & TEAM STRUCTURE",
+            col1: `Simulate any game of our choice onto Minecraft by using Python. And submit final report`,
+            col2: `Team of 3, including 3 as developers, and 1 for handling the Final Report`
         },
         {
             component: "ProjectDescription",
-            title: "Overview",
-            col1: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's 
-            standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-            It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
-            It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
-            and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
-            "discovered the undoubtable source.Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of \n de Finibus Bonorum et Malorum 
-            (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. 
-            The first line of Lorem Ipsum, Lorem ipsum dolor sit amet.., comes from a line in section`,
+            title: "EXPERIENCE",
+            col1: `This is a project that I would never ever forget. That is the first time ever that I actually programming something :)
+            For this project, our task is to simulate any game of our choice onto Minecraft by using Python. This should be pretty easy by the level that I am at right now. But back then, it was a really tough challenge, because none of us are experienced with writing any code before. Luckily, using Python was mitigate those challenges a lot because supported functions and libraries.
+            We encounter roadblocks on every step we do (except for the planning phase because I guess we are never going to run out of any idea :D ), and finally, we succeed in simulating 3 games: Tetris, Maze generating, and quick math calculating.
+            As just a school project, I don't really have any images to put in here :) so I would like to put here some random images :) I hope you enjoy it`,
         },
         {
             component: "ImageWithTitle",
-            src1: PIXEL_ARTS[getRndInteger(0, PIXEL_ARTS.length - 1)],
-            description1: "desc1",
-        },
-        {
-            component: "ProjectDescription",
-            title: "Overview",
-            col1: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's 
-            standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-            It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
-            It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
-            and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
-            "discovered the undoubtable source.Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of \n de Finibus Bonorum et Malorum 
-            (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. 
-            The first line of Lorem Ipsum, Lorem ipsum dolor sit amet.., comes from a line in section`,
-            col2: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.\n\n It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum`,
+            src1: PIXEL_ARTS[
+                getRndInteger(0, PIXEL_ARTS.length - 1)
+            ],
+            description1: "Random image",
         },
     ],
     card1: {
         title: "Credit",
-        detail: "FEATURING BAO DANG KIM, HOANG VAN THIEN, DINH QUY TRI THONG, TRAN MINH THU, BUI NGUYEN MAI TRUC, BAO PHAM.",
-    },
-    card2: {
-        title: "Open Link",
-        detail: "YOU CAN WATCH MORE ABOUT THE PROJECT VIA: YOUTUBE, GOOGLE, VNEXPRESS",
+        detail: "FEATURING BAO DANG KIM, BAO PHAM, NGO MINH THONG, TRUONG MINH HIEU.",
     },
 };
---
+++ src/mock/bao-kim/projects/daad.js
@@ -0,0 +1,33 @@
+import placeholder from "@/mock/bao-kim/images/placeholder.jpg";
+import { CHARACTER_IMAGES, getRndInteger, PIXEL_ARTS } from "@/constants";
+
+export default {
+    name: "Games simulating on Minecraft",
+    client: "School subject",
+    role: "Developer",
+    contents: [
+        {
+            component: "ProjectDescription",
+            title: "THE PROGRAM",
+            col1: `Simulate any game of our choice onto Minecraft by using Python. And submit final report`,
+            col2: `Team of 3, including 3 as developers, and 1 for handling the Final Report`
+        },
+        {
+            component: "ProjectDescription",
+            title: "EXPERIENCE",
+            col1: `Sorry for interrupting the mood, but for this one, I would like to use beloved mother-tounge Vietnamese to fully express my thought, my feeling, and of course my experience during this trip. This one would get a bit more personal, so it might not contain any of my professional information. So proceed as you please :)
+            Hi everyone :) Đây sẽ là một bài viết mang tính kể lại những trải nghiệm, hành trình mình đã trải qua ở Đức, I hope you will enjoy it.
+            Trước khi bắt đầu đi sâu hơn về trải nghiệm, thì mình xin kể 1 xíu về background của cuộc hành trình này. VGU là 1 ngôi trường đặc biệt, trường Vietnam nhưng có liên kết với những ngôi trường ở Đức, cho nên là với mỗi ngành, trường sẽ có những gói học bổng để những anh chàng sinh viên nghèo như mình có cơ hội được mở rộng tầm mắt và trải nghiệm văn hóa ở nơi xứ người. Mình đã may mắn có được 1 phần học bổng full cover chuyến đi này. Và cũng chính chuyến đi này, đã để lại 1 trong những kỷ niệm khó quên và đáng nhớ nhất của cuộc đời mình, trong 21 năm học tập tại Vietnam. Tại sao mình lại thấy nó có ý nghĩa và đặc biệt vậy ? :) Mình sẽ đi dần dần từng cột mốc nhé :) `,
+        },
+
+        {
+            component: "ImageWithTitle",
+            src1: `https://photos.app.goo.gl/BHSU2uWTE7Ssdbbh8`,
+            description1: "Random image",
+        },
+    ],
+    card1: {
+        title: "Credit",
+        detail: "FEATURING BAO DANG KIM, BAO PHAM, NGO MINH THONG, TRUONG MINH HIEU.",
+    },
+};
