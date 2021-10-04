0f60d86335c81283fe9d4b3361e8d5ff5133bfec
Pambao <phamgiabaogame@gmail.com>
2021-10-04 14:51:29 +0700

    commit

--- src/mock/userData.js
+++ src/mock/userData.js
@@ -134,18 +134,18 @@ const baoKim = {
 };
 
 const baoPham = {
-    headerTitle: "bao_dkim/portfolio",
+    headerTitle: "bao_pham/portfolio",
 
-    introName: "Dang Kim Bao - hello world",
+    introName: "Pham Gia Bao - hello world",
     introStatement: `currently, i am a web developer & programer who loves doing inspirational things with fresh minds. now i am working at Netcompany, but I always seek for a new chance to develop myself, and hoping that in oneday, I will be able to do that I love the most: Do creative and be inspired. 
         My main tech stack: Reactjs, Nodejs, Java, Spring, but I am always willing to learn new things `,
     introSubStatement: `Ho Chi Minh - Vietnam -
         dangkimbao1999@gmail.com -
         0933359290`,
     introDecoration: BaoPhamDecor,
-    linkedInUrl: "https://www.linkedin.com/in/bao-dang-kim-25a612184/",
-    facebookUrl: "https://www.facebook.com/bao.dangkim1999/",
-    githubUrl: "https://www.instagram.com/dangkimbao1999/",
+    linkedInUrl: "https://www.linkedin.com/in/bao-pham-394284185/",
+    facebookUrl: "https://www.facebook.com/giabao.pham.92167/",
+    githubUrl: "https://github.com/Pambao",
 
     timelines: [
         {
